728b8dd27a6c47cdadb090afc0ac473771d65cce
radabaugh <radabaughtyler24@gmail.com>
2019-04-26 14:42:53 -0700

    clean up

--- app/floxBot.rb
+++ app/floxBot.rb
@@ -3,11 +3,11 @@ require './modules/commands.rb'
 
 CONFIG = JSON.parse(IO.read('./config/config.json')).freeze
 
-  bot = Discordrb::Commands::CommandBot.new(
-    token: CONFIG['token'],
-    client_id: CONFIG['client_id'],
-    prefix: '!'
-  )
+bot = Discordrb::Commands::CommandBot.new(
+  token: CONFIG['token'],
+  client_id: CONFIG['client_id'],
+  prefix: '!'
+)
 
-  bot.include_commands(Commands)
-  bot.run
+bot.include_commands(Commands)
+bot.run
