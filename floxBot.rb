b2f6c66ae7cc1dbd093306efe25e7fa4e5e429a1
radabaugh <radabaughtyler24@gmail.com>
2019-04-26 14:10:39 -0700

    Modularize commands

--- floxBot.rb
+++ floxBot.rb
@@ -1,16 +1,13 @@
 require 'discordrb'
+require './commands.rb'
 
 CONFIG = JSON.parse(IO.read('config.json')).freeze
 
-module FloxBot
-  module Commands
+  bot = Discordrb::Commands::CommandBot.new(
+    token: CONFIG['token'],
+    client_id: CONFIG['client_id'],
+    prefix: '!'
+  )
 
-    bot = Discordrb::Commands::CommandBot.new(
-      token: CONFIG['token'],
-      client_id: CONFIG['client_id'],
-      prefix: '!'
-    )
-
-    bot.run
-  end
-end
+  bot.include_commands(Commands)
+  bot.run
