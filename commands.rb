3c4680519d99d6d2044e8078a9125e26a94f39b7
radabaugh <radabaughtyler24@gmail.com>
2019-04-26 14:09:24 -0700

    Update command descriptions

--- commands.rb
+++ commands.rb
@@ -1,12 +1,12 @@
 module Commands
   extend Discordrb::Commands::CommandContainer
 
-  desc = "pong!"
+  desc = "Shows the bot's current ping."
   command :ping, description: desc do |event|
     "pong! (#{Time.now - event.timestamp}s)"
   end
 
-  desc = 'hello!'
+  desc = 'Greets the user. This can be used as a test to see if the bot is working.'
   command :hello, description: desc do |event|
     event.respond "Hello #{ event.author.mention }, how can I help?"
   end
